6e1feeb0ef70004c9cee54d92b9e12ba7677b68c
Neeraj Sujan <29118597+sujanneeraj@users.noreply.github.com>
2022-06-09 07:02:35 +0200

    Time: 9 ms (75.32%), Space: 9.3 MB (98.62%) - LeetHub

---
+++ 137-single-number-ii/137-single-number-ii.cpp
@@ -0,0 +1,68 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        
+      //  return Soln1(nums);
+        return Soln2(nums);
+        
+    }
+    
+    
+private:
+    
+    int Soln2(vector<int> &nums) {
+        sort(nums.begin(), nums.end());
+        
+        if(nums.size() == 1) {
+            return nums[0];
+        }
+        
+        int i = 0;
+        if(nums[i] != nums[i+1]){
+            return nums[i];
+        }
+        i = nums.size()-1;
+        if(nums[i] != nums[i-1]) {
+            return nums[i];
+        }
+        
+        i = 1;
+        while(i < nums.size()) {
+            if(nums[i] != nums[i-1]) {
+                return nums[i-1];
+            }
+            i +=3;
+        }
+        
+        return -1;
+        
+        
+    }
+    
+    
+    int Soln1(vector<int>& nums) {
+        unordered_map<int, int> counter;
+        
+        for(int i = 0; i < nums.size(); i++) {
+            if(counter.find(nums[i]) != counter.end()) {
+                counter[nums[i]] +=1;
+            }
+            else {
+                counter[nums[i]] = 1;
+            }
+        }
+        
+    
+        for(auto &val : counter) {
+            if(val.second == 1) {
+                return val.first;
+            }
+           
+        }
+        
+        return -1;
+        
+        
+        
+    }
+};
