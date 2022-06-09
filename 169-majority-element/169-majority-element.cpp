69e7e7f4dcbddb29dd14150552ea5de4a8bc2a74
Neeraj Sujan <29118597+sujanneeraj@users.noreply.github.com>
2022-06-09 07:26:30 +0200

    Time: 28 ms (47.51%), Space: 19.5 MB (59.54%) - LeetHub

--- 169-majority-element/169-majority-element.cpp
+++ 169-majority-element/169-majority-element.cpp
@@ -1,6 +1,36 @@
 class Solution {
 public:
     int majorityElement(vector<int>& nums) {
+        
+        return Soln2(nums);
+        
+        
+        
+    }
+    
+private:
+    
+    int Soln2(vector<int> &nums) {
+     
+        int majority = nums[0];
+        int count = 1;
+        
+        for(int i = 1; i < nums.size(); i++) {
+            if(nums[i] == majority) {
+                count +=1;
+            }
+            else {
+                count -=1;
+                if(count == 0) {
+                    majority = nums[i];
+                    count = 1;
+                }
+            }
+        }
+        return majority;
+    }
+    
+    int Soln1(vector<int> &nums) {
         unordered_map<int, int> counter;
         
         for(int i = 0; i < nums.size(); i++) {
